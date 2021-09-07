ef72bd5d5cddeaf676e70fb9e60abaaca18affcf
Phil Proctor <philliptproctor@gmail.com>
2021-09-06 21:57:37 -0400

    Correctly set CSIVERSION on workflow

--- xbuild.sh
+++ xbuild.sh
@@ -9,4 +9,5 @@ docker buildx use cross-builder
 
 # Build the image
 docker buildx build --platform linux/arm64,linux/amd64,linux/arm/v7 \
+    --build-arg CSIVERSION="0.0.1-alpha3" \
     -f dist.dockerfile -t "zed-csi-local" .
